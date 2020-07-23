f633caedcac3364376dc3f781f9d58f7e5134e96
Joshua Abraham <sinisterpatrician@gmail.com>
2020-07-22 22:38:22 -0400

    Add timeout to poll to fire watchdog timer

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use mio::net::UnixDatagram;
 use mio::{Events, Interest, Poll, Token};
 
 //use std::os::unix::net::UnixDatagram;
-use std::error::Error;
+use std::time::Duration;
 use std::os::unix::io::FromRawFd;
 use std::os::unix::io::IntoRawFd;
 
@@ -27,18 +27,29 @@ fn main() {
     let mut fds = activation::receive_descriptors(false).expect("Error receiving descriptors");
     info!("Received fds: {:?}", fds);
 
-    let mut unk_sock = unsafe { UnixDatagram::from_raw_fd(fds.remove(1).into_raw_fd()) };
-    poll.registry().register(&mut unk_sock, DEVLOG, Interest::READABLE).unwrap();
+    let raw_fd = fds.remove(1).into_raw_fd();
+    info!("Creating from {}", raw_fd);
+    let mut devlog_sock = unsafe { UnixDatagram::from_raw_fd(raw_fd) };
+    poll.registry().register(&mut devlog_sock, DEVLOG, Interest::READABLE).unwrap();
 
-    let mut sdlog_sock = unsafe { UnixDatagram::from_raw_fd(fds.remove(2).into_raw_fd()) };
+    let raw_fd = fds.remove(2).into_raw_fd();
+    info!("Creating from {}", raw_fd);
+    let mut sdlog_sock = unsafe { UnixDatagram::from_raw_fd(raw_fd) };
     poll.registry().register(&mut sdlog_sock, SDJLOG, Interest::READABLE).unwrap();
 
+    let raw_fd = fds.remove(0).into_raw_fd();
+    info!("Creating from {}", raw_fd);
+    let mut unk_sock = unsafe { UnixDatagram::from_raw_fd(raw_fd) };
+    poll.registry().register(&mut unk_sock, AUDLOG, Interest::READABLE).unwrap();
+
+    info!("Sending initial notify");
     daemon::notify(false, &[daemon::NotifyState::Ready]).unwrap();
 
     loop {
-        poll.poll(&mut events, None).unwrap();
+        poll.poll(&mut events, Some(Duration::from_secs(30))).unwrap();
+        info!("Notifying watchdog");
+        daemon::notify(false, &[daemon::NotifyState::Watchdog]).unwrap();
 
-        info!("Got event...");
         for event in events.iter() {
             match event.token() {
                 SDJLOG => {
@@ -53,6 +64,15 @@ fn main() {
                 DEVLOG => {
                     let mut buf = [0u8; 1024];
                     info!("Got read event on /dev/log");
+                    devlog_sock.recv(&mut buf).expect("Error recieving");
+                    match std::str::from_utf8(&buf[..]) {
+                        Ok(s) => info!("Recieved {}", s),
+                        Err(_) => continue,
+                    };
+                }
+                AUDLOG => {
+                    let mut buf = [0u8; 1024];
+                    info!("Got read event on unk fd");
                     unk_sock.recv(&mut buf).expect("Error recieving");
                     match std::str::from_utf8(&buf[..]) {
                         Ok(s) => info!("Recieved {}", s),
